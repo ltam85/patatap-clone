7094b86882bc16e6551360518f4cc4965f4389a8
ltam85 <lawret1@uci.edu>
2018-07-05 21:30:04 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Patatap Clone Project from Colt Steele's "The Web Developer Bootcamp"
+	- Link : https://www.udemy.com/the-web-developer-bootcamp/learn/v4/
+	- A clone of the patatap website with basic JavaScript, CSS and HTML
+	- Each letter on the keyboard is bound to a different sound
+	- Each letter on the keyboard is bound to a different color circle
+	- When pressed, the sound is made and the circle will appear, shrinking away over time
---
+++ data.js
@@ -0,0 +1,158 @@
+var keyData = {
+	q: {
+		color: "#bcf45c",
+		sound: new Howl({ 
+			src: ["sounds/bubbles.mp3"]
+		})
+	},
+	w: {
+		color: "#742203",
+		sound: new Howl({ 
+			src: ["sounds/clay.mp3"]
+		})
+	},
+	e: {
+		color: "#2c224e",
+		sound: new Howl({ 
+			src: ["sounds/confetti.mp3"]
+		})
+	},
+	r: {
+		color: "#40eb04",
+		sound: new Howl({ 
+			src: ["sounds/corona.mp3"]
+		})
+	},	
+	t: {
+		color: "#dba7bb",
+		sound: new Howl({ 
+			src: ["sounds/dotted-spiral.mp3"]
+		})
+	},
+	y: {
+		color: "#34c4e4",
+		sound: new Howl({ 
+			src: ["sounds/flash-1.mp3"]
+		})
+	},
+	u: {
+		color: "#2b5508",
+		sound: new Howl({ 
+			src: ["sounds/flash-2.mp3"]
+		})
+	},
+	i: {
+		color: "#a59bdd",
+		sound: new Howl({ 
+			src: ["sounds/flash-3.mp3"]
+		})
+	},
+	o: {
+		color: "#7f0b60",
+		sound: new Howl({ 
+			src: ["sounds/glimmer.mp3"]
+		})
+	},
+	p: {
+		color: "#90beaa",
+		sound: new Howl({ 
+			src: ["sounds/moon.mp3"]
+		})
+	},
+	a: {
+		color: "#445982",
+		sound: new Howl({ 
+			src: ["sounds/pinwheel.mp3"]
+		})
+	},
+	s: {
+		color: "#3ad4ee",
+		sound: new Howl({ 
+			src: ["sounds/piston-1.mp3"]
+		})
+	},
+	d: {
+		color: "#f25317",
+		sound: new Howl({ 
+			src: ["sounds/piston-2.mp3"]
+		})
+	},
+	f: {
+		color: "#90cb1e",
+		sound: new Howl({ 
+			src: ["sounds/piston-3.mp3"]
+		})
+	},
+	g: {
+		color: "#1da432",
+		sound: new Howl({ 
+			src: ["sounds/prism-1.mp3"]
+		})
+	},
+	h: {
+		color: "#682c71",
+		sound: new Howl({ 
+			src: ["sounds/prism-2.mp3"]
+		})
+	},
+	j: {
+		color: "#71c6de",
+		sound: new Howl({ 
+			src: ["sounds/prism-3.mp3"]
+		})
+	},	
+	k: {
+		color: "#549624",
+		sound: new Howl({ 
+			src: ["sounds/zig-zag.mp3"]
+		})
+	},
+	l: {
+		color: "#e3af60",
+		sound: new Howl({ 
+			src: ["sounds/splits.mp3"]
+		})
+	},
+	z: {
+		color: "#4547e5",
+		sound: new Howl({ 
+			src: ["sounds/squiggle.mp3"]
+		})
+	},
+	x: {
+		color: "#ecc9e0",
+		sound: new Howl({ 
+			src: ["sounds/strike.mp3"]
+		})
+	},
+	c: {
+		color: "#ee7795",
+		sound: new Howl({ 
+			src: ["sounds/suspension.mp3"]
+		})
+	},
+	v: {
+		color: "#8ba632",
+		sound: new Howl({ 
+			src: ["sounds/timer.mp3"]
+		})
+	},
+	b: {
+		color: "#baa25b",
+		sound: new Howl({ 
+			src: ["sounds/ufo.mp3"]
+		})
+	},
+	n: {
+		color: "#d338e0",
+		sound: new Howl({ 
+			src: ["sounds/veil.mp3"]
+		})
+	},
+	m: {
+		color: "#bc0d91",
+		sound: new Howl({ 
+			src: ["sounds/wipe.mp3"]
+		})
+	}
+}
